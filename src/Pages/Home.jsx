7b39064cb446a469d3f254044f30e15509c7fede
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-10-23 13:20:27 +0530

    conflict solved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,34 +9,18 @@ import MeetTeam from '../Components/specific/Home/MeetTeam'
 
 import WhatWeDo from '../Components/specific/Home/WhatWeDo'
 import Testimonials from '../Components/specific/Home/Testimonials'
-=======
 import BookAnAppoinment from '../Components/specific/Home/BookAnAppoinment'
 
 
 const Home = () => {
   return (
     <div  id='HomePage' className=' bg-[#0B675A] w-full  font-poppins relative overflow-x-hidden '>
-
-
       <Circle />
-      {/* < Header/> */}
       <HeroSection /> 
       <HomeAboutUs/>
       <WhyChooseUsHome />
       <WhatWeDo />
-      <Testimonials />
-      {/* <HeroSection /> 
-      <HomeAboutUs/> */}
-      {/* <MeetTeam /> */}
-
-       {/* <Circle />
-      < Header/>
-      <HeroSection /> 
-      <HomeAboutUs/>
-      // <WhyChooseUsHome /> */}
-      {/* // <MeetTeam />  */}
       <BookAnAppoinment/>
-
     </div>
   )
 }
